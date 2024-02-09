96b31ceec4e6550dfbab9556188fccf4d118eecf
LucasCscBks <luke100191@gmail.com>
2024-02-09 18:05:38 -0300

    About view

--- src/main/java/com/studies/application/App.java
+++ src/main/java/com/studies/application/App.java
@@ -14,24 +14,30 @@ import java.nio.file.Paths;
  */
 public class App extends Application {
 
+    private static Scene mainScene;
+
     @SuppressWarnings("exports")
     @Override
     public void start(Stage primaryStage) {
         try {
             String path = "src/main/java/com/studies/gui/MainView.fxml";
-            System.out.println("Diretório de trabalho: " + System.getProperty("user.dir"));
             ScrollPane scrollPane = FXMLLoader.load(Paths.get(path).toUri().toURL());
             scrollPane.setFitToHeight(true);
             scrollPane.setFitToWidth(true);
 
 
-            Scene mainScene = new Scene(scrollPane);
+            mainScene = new Scene(scrollPane);
             primaryStage.setScene(mainScene);
             primaryStage.setTitle("Sample JavaFX application");
             primaryStage.show();
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    @SuppressWarnings("exports")
+    public static Scene getMainScene() {
+        return mainScene;
     }
 
 
